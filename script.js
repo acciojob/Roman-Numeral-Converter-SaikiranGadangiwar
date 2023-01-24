bbd5e762429a9348ba7bea8a9d8902a1a1ee1634
SaikiranGadangiwar <106570439+SaikiranGadangiwar@users.noreply.github.com>
2023-01-24 11:39:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -10,21 +10,30 @@ function convertToRoman(num) {
     };
 
   //your code here
-
-	 
-  let roman = '';
-  for (let key in romanNumerals) {
-    while (num >= romanNumerals[key]) {
-      roman += key;
-      num -= romanNumerals[key];
+ let roman = "";
+    let i = 0;
+    while (num > 0) {
+        while (num >= romanNumerals[i][1]) {
+            roman += romanNumerals[i][0];
+            num -= romanNumerals[i][1];
+        }
+        i++;
     }
-  }
-  return roman;
+    return roman;
+	 
+  // let roman = '';
+  // for (let key in romanNumerals) {
+  //   while (num >= romanNumerals[key]) {
+  //     roman += key;
+  //     num -= romanNumerals[key];
+  //   }
+  // }
+  // return roman;
 
 }
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
- console.log(convertToRoman(36));
+ //console.log(convertToRoman(36));
 
 
 
